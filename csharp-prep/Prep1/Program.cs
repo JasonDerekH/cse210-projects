0f4d5bbeac060691da68f9d7752eeddd2cdbb952
JasonDerekH <158780101+JasonDerekH@users.noreply.github.com>
2024-11-02 19:38:44 -0600

    Added prompts to ask and print name.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep1 World!\n");
+
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {last}, {first} {last}.");
     }
 }
